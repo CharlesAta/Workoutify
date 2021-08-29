e02c34eab3dec61ed08f99310c2cf4e9bc29e656
Charles Ata <charlesata@hotmail.ca>
2021-08-29 18:41:05 -0400

    Delete old key. Generate new hidden key

---
+++ workoutify/workoutify/secret_settings.py
@@ -0,0 +1,1 @@
+SECRETKEY="&waul%sv&%51it+r8jmnsg)rb(3+tl3@@yf1g_p%tfy67ea4+7"
--- workoutify/workoutify/settings.py
+++ workoutify/workoutify/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from secret_settings import SECRETKEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s)gin%6857!jkhe)m=j@ldgc&i=i-)(b4_5l$4(6ji#3#t=g&p'
+SECRET_KEY = SECRETKEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
